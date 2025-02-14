9ba3a5a496667061a804e725f500a802190cd8f3
Rishav Kumar <rishavkumar18902@gmail.com>
2025-02-14 20:24:30 +0530

    Feat: #5 Installed Tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <video src="https://brilliant.org/videos/homepage/learn-by-doing.webm" loop autoPlay muted></video>
+    </div>
+  )
+}
+
+export default Home
