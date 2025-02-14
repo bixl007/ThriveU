fbd9306e577d3784a5db57fa0af78df1fed7390e
Rishav Kumar <rishavkumar18902@gmail.com>
2025-02-15 01:50:13 +0530

    Loading animation section added

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,17 +1,25 @@
-
-import Home from '../components/Home'
-import Marquee from '../components/Marquee'
-
-
+import { useState, useEffect } from "react";
+import Home from "../components/Home";
+import Marquee from "../components/Marquee";
+import LoadingAnimation from "../components/LoadingAnimation"; // Import animation component
 
 function Landing() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000); // Simulating a loading time
+  }, []);
+
   return (
     <div>
-      <Home />
-      <Marquee />
-
+      {loading ? <LoadingAnimation /> : (
+        <>
+          <Home />
+          <Marquee />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
