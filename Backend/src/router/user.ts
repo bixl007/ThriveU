c9a1a8fb27124eab4586f7d161a42f8916cdc85f
Bishal Baira <bishalbaira432@gmail.com>
2025-02-14 23:11:53 +0530

    added auth routes

--- Backend/src/router/user.ts
+++ Backend/src/router/user.ts
@@ -10,76 +10,81 @@ const router = Router();
 
 // @ts-ignore
 router.post("/signup", async (req: Request, res: Response) => {
-    const body = req.body;
-    const parsedData = SignupSchema.safeParse(body);
-  
-    if (!parsedData.success) {
-      return res.status(400).json({
-        message: "Invalid data",
-      });
-    }
-  
-    const userExists = await prismaClient.user.findFirst({
-      where: {
-        email: parsedData.data.username,
-      },
+  const body = req.body;
+  const parsedData = SignupSchema.safeParse(body);
+
+  if (!parsedData.success) {
+    return res.status(400).json({
+      message: "Invalid data",
     });
-  
-    if (userExists) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
-    }
-  
-    await prismaClient.user.create({
-      data: {
-        email: parsedData.data.username,
-        password: parsedData.data.password, // TODO: hash password
-        name: parsedData.data.name,
-      },
-    });
-  
-    // await sendEmail();
-  
-    return res.json({
-      message: "Please verify your account by checking your email",
-    });
-  });
-  
-  // @ts-ignore
-  router.post("/signin", async (req: Request, res: Response) => {
-    const body = req.body;
-    const parsedData = SigninSchema.safeParse(body);
-  
-    if (!parsedData.success) {
-      return res.status(400).json({
-        message: "Invalid data",
-      });
-    }
-  
-    const user = await prismaClient.user.findFirst({
-      where: {
-        email: parsedData.data.username,
-        password: parsedData.data.password,
-      },
-    });
-  
-    if (!user) {
-      return res.status(400).json({
-        message: "Invalid credentials",
-      });
-    }
-  
-    const token = jwt.sign(
-      {
-        id: user.id,
-      },
-      JWT_PASSWORD
-    );
-  
-    return res.json({
-      token,
-    });
+  }
+
+  const userExists = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.username,
+    },
   });
 
-  export const userRouter = router;
+  if (userExists) {
+    return res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email: parsedData.data.username,
+      password: parsedData.data.password, // TODO: hash password
+      name: parsedData.data.name,
+    },
+  });
+
+  return res.json({
+    message: "Signed up",
+  });
+});
+
+// @ts-ignore
+router.get("/signup", (req, res) => {
+  return res.json({
+    message: "Hello World",
+  });
+});
+
+// @ts-ignore
+router.post("/signin", async (req: Request, res: Response) => {
+  const body = req.body;
+  const parsedData = SigninSchema.safeParse(body);
+
+  if (!parsedData.success) {
+    return res.status(400).json({
+      message: "Invalid data",
+    });
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.username,
+      password: parsedData.data.password,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    JWT_PASSWORD
+  );
+
+  return res.json({
+    token,
+  });
+});
+
+export const userRouter = router;
